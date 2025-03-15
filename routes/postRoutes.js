427255a3fec146af7e695a2d004815f64cc3f4aa
Your Name <naseribrahim755@gmail.com>
2025-03-15 17:11:23 -0500

    removed the json returning authdata

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -19,10 +19,6 @@ postRouter.post("/", verifyToken, async (req, res) => {
     jwt.verify(req.token, process.env.SECRET_KEY, (err, authData) => {
         if(err) {
             res.sendStatus(403);
-        } else {
-            res.json({
-                authData
-            })
         }
     })
     const result = await addPost(req.body.title, req.body.content);
